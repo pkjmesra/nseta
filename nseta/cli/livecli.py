a27ed29051fc1602ded47427fab79c476c50f7ce
PK <pkjmesra@gmail.com>
2020-10-06 13:26:59 +0530

    Updated for better readability

--- nseta/cli/livecli.py
+++ nseta/cli/livecli.py
@@ -5,6 +5,8 @@ from nseta.live.live import get_quote
 from nseta.cli.inputs import *
 from datetime import datetime
 
+QUOTE_KEYS = ['companyName', 'lastPrice', 'pChange', 'open', 'dayHigh', 'dayLow', 'closePrice', 'previousClose', 'high52', 'low52']
+VOLUME_KEYS = ['quantityTraded', 'totalTradedVolume', 'totalTradedValue']
 
 @click.command(help='Get live price quote of a security')
 @click.option('--symbol', '-S',  help='Security code')
@@ -19,57 +21,35 @@ def live_quote(symbol, series):
         return
     data = result['data'][0]
     time = result['lastUpdateTime']
-    company_name = data['companyName']
-    last_trade_price = data['lastPrice']
-    price_change = data['pChange']
-    o = data['open']
-    h = data['dayHigh']
-    l = data['dayLow']
-    c = data['closePrice']
-    prev_close = data['previousClose']
-    wk52_high = data['high52']
-    wk52_low = data['low52']
-    quote_data = [[company_name, last_trade_price, price_change, o, h, l, c, prev_close, wk52_high, wk52_low]]
+    quote_data = []
+    for key in QUOTE_KEYS:
+        quote_data.append(data[key])
+    quote_data = [quote_data]
 
-    traded_qty = data['quantityTraded']
-    traded_volume = data['totalTradedVolume']
-    traded_value = data['totalTradedValue']
-    volume_data = [[traded_qty, traded_volume, traded_value]]
+    volume_data = []
+    for key in VOLUME_KEYS:
+        volume_data.append(data[key])
+    volume_data = [volume_data]
 
-    sellQuantity5 = data['sellQuantity5']
-    sellQuantity4 = data['sellQuantity4']
-    sellQuantity3 = data['sellQuantity3']
-    sellQuantity2 = data['sellQuantity2']
-    sellQuantity1 = data['sellQuantity1']
-    buyQuantity5 = data['buyQuantity5']
-    buyQuantity4 = data['buyQuantity4']
-    buyQuantity3 = data['buyQuantity3']
-    buyQuantity2 = data['buyQuantity2']
-    buyQuantity1 = data['buyQuantity1']
-    
-    buyPrice1 = data['buyPrice1']
-    buyPrice2 = data['buyPrice2']
-    buyPrice3 = data['buyPrice3']
-    buyPrice4 = data['buyPrice4']
-    buyPrice5 = data['buyPrice5']
-    sellPrice1 = data['sellPrice1']
-    sellPrice2 = data['sellPrice2']
-    sellPrice3 = data['sellPrice3']
-    sellPrice4 = data['sellPrice4']
-    sellPrice5 = data['sellPrice5']
-    pipeline_data = [[buyQuantity1, buyPrice1, sellQuantity1, sellPrice1],
-                    [buyQuantity2, buyPrice2, sellQuantity2, sellPrice2],
-                    [buyQuantity3, buyPrice3, sellQuantity3, sellPrice3],
-                    [buyQuantity4, buyPrice4, sellQuantity4, sellPrice4],
-                    [buyQuantity5, buyPrice5, sellQuantity5, sellPrice5]]
+    pipeline_data = []
+    for x in range(1,5):
+        buy_qty_key = 'buyQuantity' + str(x)
+        buy_prc_key = 'buyPrice' + str(x)
+        sell_qty_key = 'sellQuantity' + str(x)
+        sell_prc_key = 'sellPrice' + str(x)
+        columns = [buy_qty_key, buy_prc_key, sell_qty_key, sell_prc_key]
+        row = []
+        for column in columns:
+            row.append(data[column])
+        pipeline_data.append(row)
 
-    dfquote = pd.DataFrame(quote_data, columns = ['Name', 'Last Trade Price', 'Price Change', 
-        'Open', 'High', 'Low', 'Close', 'Prev Close', '52 wk High', '52 wk Low'])
+    dfquote = pd.DataFrame(quote_data, columns = ['Name', 'Last_Trade_Price', 'Price_Change', 
+        'Open', 'High', 'Low', 'Close', 'Prev_Close', '52wk_High', '52wk_Low'])
     dfquote.set_index('Name', inplace=True)
-    dfvolume = pd.DataFrame(volume_data, columns = ['Quantity Traded','Total Traded Volume', 'Total Traded Value'])
-    dfvolume.set_index('Quantity Traded', inplace=True) 
-    dfpipeline = pd.DataFrame(pipeline_data, columns = ['Bid Quantity','Bid Price', 'Offer Quantity', 'Offer Price'])
-    dfpipeline.set_index('Bid Quantity', inplace=True) 
+    dfvolume = pd.DataFrame(volume_data, columns = ['Quantity_Traded','Total_Traded_Volume', 'Total_Traded_Value'])
+    dfvolume.set_index('Quantity_Traded', inplace=True) 
+    dfpipeline = pd.DataFrame(pipeline_data, columns = ['Bid_Quantity','Bid_Price', 'Offer_Quantity', 'Offer_Price'])
+    dfpipeline.set_index('Bid_Quantity', inplace=True) 
     print ('As of ' + time + '\n')
     print (dfquote.head())
     print ('\n')
