b52f11daf4cfb50f54fa8d3f6dc07334c758a549
PK <pkjmesra@gmail.com>
2021-02-20 17:46:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
 	"Operating System :: OS Independent",
 	'Programming Language :: Python',
 	'Programming Language :: Python :: 3',
-	'Programming Language :: Python :: 3.7'
+	'Programming Language :: Python :: 3.7',
 	'Programming Language :: Python :: 3.8',
 	],
 	install_requires = install_requires,
